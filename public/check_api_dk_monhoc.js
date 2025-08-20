495269949be0428c45892174d6e583bc2b8278d9
Nguyễn Duy Hùng <149739374+ngDuyHung@users.noreply.github.com>
2025-08-21 00:34:26 +0700

    Delete public/check_api_dk_monhoc.js
    dsd

--- public/check_api_dk_monhoc.js
+++
@@ -1,6 +0,0 @@
-export default function handler(req, res) {
-  const { key } = req.query;
-  const validKey = "STU2025-REGISTER-VALID"; // Key thật, chỉ lưu ở đây
-
-  res.status(200).json({ valid: key === validKey });
-}
